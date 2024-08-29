d2912d66c0a8b3e1d22b203d08014f6b567beab9
Lais Telles <128053913+lais-telles@users.noreply.github.com>
2024-08-29 09:15:00 -0300

    funções matemáticas

---
+++ src/funçãomatematica.kt
@@ -0,0 +1,11 @@
+import kotlin.math.*
+
+fun main () {
+    var x: Float = 2.0f
+
+    println(max(5, 7))
+    println(min(5, 7))
+    println(sqrt(81.0f))
+    println(x.pow(2))
+    println(round(76.873))
+}
